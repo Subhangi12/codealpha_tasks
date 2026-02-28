931e74a29f7ce51006ee09b531aa912e7cbcb452
Subhangi12 <kanthasubhangi6@gmail.com>
2026-02-28 17:53:56 +0530

    Delete practice3.py

--- practice3.py
+++
@@ -1,4 +0,0 @@
-num1=int(input("enter value of num1:"))
-convertedvalue=float(num1)
-print("original value is",num1,"datatype",type(num1))
-print("convertedvalue is",convertedvalue,"datatype",type(convertedvalue))
